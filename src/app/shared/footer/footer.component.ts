ba2ea1c0669ce573ea64ec3021a4db3454019b49
marchilla <miguelarchilla93@gmail.com>
2017-11-03 23:31:34 +0100

    Added generic footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
@@ -8,17 +7,18 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
     contactInfo = new Contact();
-    
-}
 
+}
 
 class Contact {
     name: string = 'FeelFood Company'
+    address: string = 'Esteve Terradas, 7'
     phone: string = '+32 000 000 000'
     email: string = 'info@feelfood.es'
+    facebook: string = 'https://www.facebook.com/delegacio.eetac/'
     github: string = 'https://github.com/FeelFood'
-    twitter: string;
+    twitter: string = 'https://twitter.com/ea_eetac/status/911256516937834496'
     country: string = 'Spain'
     city: string = 'Barcelona'
-    legalInfo: string = 'This is a private project created by EA students in EETAC.'    
+    legalInfo: string = 'This is a private project created by EA students in EETAC. All rights are reserved by their developers.'
 }
