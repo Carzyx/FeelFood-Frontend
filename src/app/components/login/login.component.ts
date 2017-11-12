b87746cff1bce674753a824b5148f54b25db2140
marchilla <miguelarchilla93@gmail.com>
2017-11-12 16:42:29 +0100

    Fixed Login request

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import "rxjs/add/operator/map";
 import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
 import { User } from '../../models/user';
 
@@ -12,19 +13,19 @@ import { User } from '../../models/user';
 export class LoginComponent implements OnInit {
   private data: User;
   constructor(private http: HttpClient) {
-    this.data = new User(); 
-  } 
+    this.data = new User();
+  }
   ngOnInit() {
   }
-  
 
   loginSubmit(username, email, password) {
+    
     this.data.username = username
     this.data.email = email
     this.data.password = password;
 
-    this.http.post('http://localhost:3001/user', JSON.stringify(this.data))
-      .subscribe();
-    alert("loginSubmit done! " + JSON.stringify(this.data))
+    this.http.post('http://localhost:3001/register', JSON.stringify(this.data), { headers: new HttpHeaders().set('Content-Type', 'application/json') })
+      .subscribe(data => console.log(data));
   }
+
 }
